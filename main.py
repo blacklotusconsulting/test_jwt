d6ee7a47422717c654be82048d8c01804cd31071
blacklotusconsulting <info@blacklotus.eu>
2023-07-26 14:58:39 +0200

    aa

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # This is a sample Python script.
 import jwt
+import requests
 
 
 def encode_user():
@@ -8,11 +9,20 @@ def encode_user():
     :param user:
     :return:
     """
-    encoded_data = jwt.encode(payload={"name": "Alessandro","cognome": "Romani", "palazzo": 1},
+    encoded_data = jwt.encode(payload={"name": "Alessandro", "cognome": "Romani", "palazzo": 1},
                               key='bubu1aa',
                               algorithm="HS256")
     return encoded_data
-
+def request_bubu():
+    r = requests.get('https://httpbin.org/basic-auth/user/pass', auth=('user', 'pass'))
+    return r.status_code
+def post_bubu():
+    data = '11okokok'
+    p = requests.post('https://httpbin.org/post',data)
+    return p.status_code
 
 if __name__ == "__main__":
-    print(encode_user())
+    encoded_data = encode_user()
+    print("Encoded data:", encoded_data.decode())  # Decoding bytes to a string
+    print("HTTP status code:", request_bubu())
+    print("HTTP status code:", post_bubu())
